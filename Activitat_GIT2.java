87122904db77d29cae679bd390a0ba6bdfb6063f
Blanco <gerardblanco2006@gmail.com>
2024-12-23 06:36:40 +0100

    1

--- Activitat_GIT2.java
+++ Activitat_GIT2.java
@@ -9,23 +9,7 @@ public class Activitat_GIT2 {
  
         //matriz-inicial
 
-        int[][] array = new int[5][5];
-
-
-        for (int vueltas = 0; vueltas < array.length; vueltas++) {
-            for (int vueltas2 = 0; vueltas2 < array[vueltas].length; vueltas2++) {
-            
-                
-                Random rand = new Random(); 
-                
-                int aleatorio = rand.nextInt(101); 
-                
-                array[vueltas][vueltas2] = aleatorio;
-                
-                
-            }
-        }
-
+        
 
 
 
