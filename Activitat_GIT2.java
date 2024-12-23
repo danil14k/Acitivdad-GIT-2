c66802f40029a70bb0af6a2ba5814068eb070682
Blanco <gerardblanco2006@gmail.com>
2024-12-23 07:00:18 +0100

    busqueda numero

--- Activitat_GIT2.java
+++ Activitat_GIT2.java
@@ -15,7 +15,30 @@ public class Activitat_GIT2 {
         
         //busqueda-numero
         
-      
+        int igual = 0;  
+              
+        System.out.println("¿Qué número quieres comprobar?");
+        int numero = scan_numeros.nextInt();
+        
+        for (int vueltas = 0; vueltas < array.length; vueltas++) {
+            for (int vueltas2 = 0; vueltas2 < array[vueltas].length; vueltas2++) {
+                if (numero == array[vueltas][vueltas2]){
+                    igual = 1;
+                    int posicion1 = vueltas;  
+                    int posicion2 = vueltas2; 
+                    break; 
+                } 
+            }
+            
+        }
+        
+        
+        if (igual == 1) {
+            System.out.println("El número coincide");
+            System.out.println("La posición del número es: " + posicion1 + "," + posicion2);
+        } else {
+            System.out.println("El número no coincide");
+        } 
 
     }
 }
