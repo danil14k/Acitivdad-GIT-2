503e949a2c8be9ac59ae4e0c7b61ea08fb2b5dcb
Blanco <gerardblanco2006@gmail.com>
2024-12-23 05:49:36 +0100

    Matriz 5x5 + scaneres

--- Activitat_GIT2.java
+++ Activitat_GIT2.java
@@ -1,7 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Activitat_GIT2 {
     public static void main(String[] args) {
         
-        System.out.println("Hola");
+        Scanner scan_numeros = new Scanner(System.in);
+        Scanner scan_letras = new Scanner(System.in);
+ 
+        String[][] array = new String[5][5];
+
+        
+        
+
+
+
 
     }
 }
